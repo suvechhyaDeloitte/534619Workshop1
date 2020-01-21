904067d3524ef2035d4986e9df482772a952be3d
Divyaranjan Deviprasad <ddeviprasad@deloitte.com>
2020-01-21 09:40:36 +0530

    initial commit

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <article class="landing-wrapper">
+    <h2>{{ welcomeMsg }}</h2>
+    <p class="app-description">
+      {{ appDesc }}
+      <a href="https://vuefest-2020.surge.sh/" target="_blank">Know more..</a>
+    </p>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "LandingPage",
+  data() {
+    return {
+      welcomeMsg: "Welcome to Mini Good Read",
+      appDesc:
+        "A sample app developed to serve the purpose of learning Vue at VueFest 2020, Deloitte."
+    };
+  }
+};
+</script>
+<style>
+.landing-wrapper {
+  padding: 5rem;
+  text-align: center;
+}
+
+.landing-wrapper h2 {
+  font-size: 7rem;
+}
+
+.landing-wrapper .app-description {
+  padding: 3rem 0;
+  font-size: 2rem;
+  text-align: center;
+  color: #44576e;
+}
+</style>
